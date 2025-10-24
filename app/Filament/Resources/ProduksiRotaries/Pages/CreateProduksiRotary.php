6dc5afa904afd3a85900215dcb600e52211964cc
Santoso <33777569+sanenchan@users.noreply.github.com>
2025-10-24 08:24:35 +0700

    refactor: Remove custom record creation logic from CreateProduksiRotary

--- app/Filament/Resources/ProduksiRotaries/Pages/CreateProduksiRotary.php
+++ app/Filament/Resources/ProduksiRotaries/Pages/CreateProduksiRotary.php
@@ -3,28 +3,9 @@
 namespace App\Filament\Resources\ProduksiRotaries\Pages;
 
 use App\Filament\Resources\ProduksiRotaries\ProduksiRotaryResource;
-use App\Models\ProduksiRotary;
 use Filament\Resources\Pages\CreateRecord;
-use Illuminate\Database\Eloquent\Model;
 
 class CreateProduksiRotary extends CreateRecord
 {
     protected static string $resource = ProduksiRotaryResource::class;
-    protected function handleRecordCreation(array $data): Model
-    {
-        $mesinIds = $data['id_mesin'];
-        unset($data['id_mesin']);
-
-        $created = null;
-
-        foreach ($mesinIds as $idMesin) {
-            $created = ProduksiRotary::create([
-                ...$data,
-                'id_mesin' => $idMesin,
-            ]);
-        }
-
-        // Return satu model terakhir agar kompatibel
-        return $created;
-    }
 }
