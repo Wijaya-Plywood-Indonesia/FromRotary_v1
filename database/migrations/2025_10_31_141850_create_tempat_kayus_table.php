4ef0642d86e6538214a39d866df32b0413a8606f
Santoso <33777569+sanenchan@users.noreply.github.com>
2025-11-03 11:10:10 +0700

    ini adalah migrations

--- database/migrations/2025_10_31_141850_create_tempat_kayus_table.php
+++ database/migrations/2025_10_31_141850_create_tempat_kayus_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,22 @@ return new class extends Migration
     {
         Schema::create('tempat_kayus', function (Blueprint $table) {
             $table->id();
+            $table->integer('jumlah_batang');
+            $table->integer('poin');
+
+            $table->foreignId('id_kayu_masuk')
+                ->nullable() // tambahkan nullable jika tabel sudah berisi data agar tidak error saat migrasi
+                ->after('poin')
+                ->constrained('kayu_masuks')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
             $table->timestamps();
         });
     }
+
+    //     'jumlah',
+    // 'poin',
+    // 'id_kayu_masuk'
 
     /**
      * Reverse the migrations.
--- database/migrations/2025_10_31_153338_create_riwayat_kayus_table.php
+++ database/migrations/2025_10_31_153338_create_riwayat_kayus_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,11 +12,25 @@ return new class extends Migration
     {
         Schema::create('riwayat_kayus', function (Blueprint $table) {
             $table->id();
+            $table->date('tanggal_masuk');
+            $table->date('tanggal_habis');
+            $table->foreignId('id_tempat_kayu')
+                ->nullable() // tambahkan nullable jika tabel sudah berisi data agar tidak error saat migrasi
+                ->after('tanggal_habis')
+                ->constrained('tempat_kayus')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
             $table->timestamps();
         });
     }
 
+    //     'tanggal_masuk',
+    // 'tanggal_digunakan',
+    // 'tanggal_habis',
+    // 'id_tempat_masuk'
     /**
+     * 
      * Reverse the migrations.
      */
     public function down(): void
