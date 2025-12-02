2dfdc16a0c6d23b02fd547438151565b75afb9f1
Dian Wicaksono <99239495+DianKartiko@users.noreply.github.com>
2025-12-02 09:35:55 +0700

    Fix migration for detail_pegawai_hp table creation

--- database/migrations/2025_11_25_141228_create_detail_pegawai_hp_table.php
+++ database/migrations/2025_11_25_141228_create_detail_pegawai_hp_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_11_25_141228_create_detail_pegawai_hp_table.php
         Schema::create('detail_pegawai_hp', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_produksi_hp')
@@ -20,31 +19,17 @@ return new class extends Migration
                 ->restrictOnDelete();
             $table->foreignId('id_mesin')
                 ->constrained('mesins')
-========
-        Schema::create('rencana_pegawais', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('id_produksi_repair')
-                ->constrained('produksi_repairs')
->>>>>>>> dian:database/migrations/2025_11_29_132716_create_rencana_pegawais_table.php
                 ->cascadeOnUpdate()
                 ->restrictOnDelete();
             $table->foreignId('id_pegawai')
                 ->constrained('pegawais')
                 ->cascadeOnUpdate()
                 ->restrictOnDelete();
-<<<<<<<< HEAD:database/migrations/2025_11_25_141228_create_detail_pegawai_hp_table.php
             $table->text('tugas');
             $table->time('masuk');
             $table->time('pulang');
             $table->string('ijin')->nullable();
             $table->string('ket')->nullable();
-========
-            $table->integer('nomor_meja');
-            $table->time('jam_masuk');
-            $table->time('jam_pulang');
-            $table->string('ijin')->nullable();
-            $table->string('keterangan')->nullable();
->>>>>>>> dian:database/migrations/2025_11_29_132716_create_rencana_pegawais_table.php
             $table->timestamps();
         });
     }
@@ -54,10 +39,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-<<<<<<<< HEAD:database/migrations/2025_11_25_141228_create_detail_pegawai_hp_table.php
         Schema::dropIfExists('detail_pegawai_hp');
-========
-        Schema::dropIfExists('rencana_pegawais');
->>>>>>>> dian:database/migrations/2025_11_29_132716_create_rencana_pegawais_table.php
     }
 };
