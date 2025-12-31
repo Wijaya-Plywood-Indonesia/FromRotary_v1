c35c16e067f58fb7dd002053f0557928314728b6
FarisEka <fariseka3@gmail.com>
2025-12-31 15:44:34 +0700

    add migration and model for produksi pot jelek, pegawai pot jelek, detail barang dikerjakan pot jelek, validasi pot jelek

---
+++ app/Models/DetailBarangDikerjakanPotJelek.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetailBarangDikerjakanPotJelek extends Model
+{
+    protected $table = 'detail_barang_dikerjakan_pot_jelek';
+
+    protected $fillable = [
+        'id_produksi_pot_jelek',
+        'id_pegawai_pot_jelek',
+        'id_ukuran',
+        'id_jenis_kayu',
+        'tinggi',
+        'kw',
+        'no_palet',
+        'jumlah',
+    ];
+
+    public function produksiPotJelek()
+    {
+        return $this->belongsTo(ProduksiPotJelek::class, 'id_produksi_pot_jelek');
+    }
+
+    public function PegawaiPotJelek()
+    {
+        return $this->belongsTo(PegawaiPotJelek::class, 'id_pegawai_pot_jelek');
+    }
+
+    public function ukuran()
+    {
+        return $this->belongsTo(Ukuran::class, 'id_ukuran');
+    }
+
+    public function jenisKayu()
+    {
+        return $this->belongsTo(JenisKayu::class, 'id_jenis_kayu');
+    }
+}
