af6f11059d642f14824a4eb163b912eb66d1954f
FarisEka <fariseka3@gmail.com>
2025-12-31 11:25:01 +0700

    add migration and model for produksi pot siku, pegawai pot siku, detail barang dikerjakan pot siku, validasi pot siku

---
+++ app/Models/PegawaiPotSiku.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PegawaiPotSiku extends Model
+{
+    protected $table = 'pegawai_pot_siku';
+
+    protected $fillable = [
+        'id_produksi_pot_siku',
+        'id_pegawai',
+        'tugas',
+        'masuk',
+        'pulang',
+        'tinggi',
+        'ijin',
+        'ket',
+    ];
+
+    public function produksiPotSiku()
+    {
+        return $this->belongsTo(ProduksiPotSiku::class, 'id_produksi_pot_siku');
+    }
+
+    public function pegawai()
+    {
+        return $this->belongsTo(Pegawai::class, 'id_pegawai');
+    }
+}
